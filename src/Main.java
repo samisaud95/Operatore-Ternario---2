70bb63096b77c80620dffd2fcab979afdd9c2820
Samira saud <samirasaudit@gmail.com>
2023-12-21 22:58:54 +0100

    Fatto

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Valore: " + i);
+            System.out.println(checkValue(i));
+        }
+
+    }
+
+    private static String checkValue(int value) {
+       // String result = "";
+
+       // if (value % 5 == 0) {
+       //     result = "FizzBuzz";
+       // } else  {
+        //    result = "Buzz";
+        //}
+        //return result;
+
+        String result = (value % 5 ==0) ? "FizzBuzz" : "Buzz";
+        return result;
+    }
+
+}
